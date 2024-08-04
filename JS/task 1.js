176d2cfd651a58eca307a14e6a1cff223aec0fc0
Softmaks <maxviruk@gmail.com>
2024-08-05 02:01:03 +0300

    11
    11

--- JS/task 1.js
+++ JS/task 1.js
@@ -18,6 +18,7 @@
 // Виклик isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14) повертає true
 // Виклик isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7) повертає false
 
+єлщ;
 console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
 
 console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
